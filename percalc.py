67c69f7d4d20f892e048c3b6154ff5b6b06467a3
SyuxCodeZ <cuberdytm@gmail.com>
2024-12-19 17:25:58 +0600

    done

---
+++ percalc.py
@@ -0,0 +1,15 @@
+#The Heading
+
+print ("~~~~~PERCENTAGE CALCULATION~~~~~")
+
+#The Input Variables
+
+obtainedmarks = int(input("Enter The First Number "))
+
+fullmarks = int(input("Enter The Last Number "))
+
+#The Variable And Print
+
+percentage = obtainedmarks/fullmarks*100
+
+print (f"{percentage}%")
