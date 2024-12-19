4949ae125fd1d69031d2277fcc44cff3ad4f2606
SyuxCodeZ <cuberdytm@gmail.com>
2024-12-19 14:51:49 +0600

    average

---
+++ Average.py
@@ -0,0 +1,19 @@
+# the variables...
+
+tree1 = 98
+tree2 = 95
+tree3 = 41
+tree4 = 11
+tree5 = 94
+
+# finding the sum
+
+sum = tree1+tree2+tree3+tree4+tree5
+
+print (sum)
+
+#the average...
+
+result = sum/5
+
+print (result)
